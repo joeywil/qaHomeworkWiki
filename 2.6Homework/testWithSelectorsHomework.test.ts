ad6c7546b320116c201c9776c2045e468384a416
Joey Wilkinson <cupofjoe_q@live.com>
2023-05-26 10:57:26 -0600

    2.6 Homework

---
+++ 2.6Homework/testWithSelectorsHomework.test.ts
@@ -0,0 +1,34 @@
+import {Builder, By, Capabilities, WebDriver} from "selenium-webdriver"
+
+const chromedriver = require("chromedriver")
+
+const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build();
+
+describe("Filling in the blanks", () => {
+    beforeEach(async () => {
+        await driver.get('https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html')
+    });
+    afterAll(async () => {
+        await driver.quit();
+    })
+
+    const hdrInput: By = By.css('[name = "hdrInput"]')
+    const mkeInput: By = By.xpath('//input[@name="mkeInput"]') 
+    const oriInput: By = By.css('[name="oriInput"]')
+    const nameInput: By = By.css('[name="namInput"]') 
+    const clrBtn: By = By.id('clearBtn') 
+    const submitBtn: By = By.id('saveBtn') 
+    const errorMsg: By = By.id('validHeader')
+
+    test("filling in the blanks for real", async () => {
+        await driver.findElement(hdrInput).sendKeys("Change this")
+        await driver.findElement(mkeInput).sendKeys("change this")
+        await driver.findElement(oriInput).sendKeys("change this")
+        await driver.findElement(nameInput).sendKeys("change this")
+        await driver.findElement(submitBtn).click()
+        let errorHdr = await driver.findElement(errorMsg).getText()
+        expect(errorHdr).toContain("Errors Received:")
+        await driver.findElement(clrBtn).click()
+    
+    });
+});
